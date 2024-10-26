eacdccbc171db2da4bb06079c522fafb9789b8c1
Rael <santosrafaelpro@gmail.com>
2024-10-26 15:15:48 -0300

    updating configs cursor

--- mathPy/Basics concepts/factorial.py
+++ mathPy/Basics concepts/factorial.py
@@ -7,3 +7,28 @@ def factorial(n):
 n = int(input("Enter a number: "))
 print(f'The factorial of this number is: [{factorial(n)}]')
 print("Ending program...")
+
+""" 
+^^ Other ways to do the same:
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# ... existing code ...
+n = int(input("Digite um número: "))
+print(f'O fatorial deste número é: [{factorial(n)}]')
+print("Encerrando o programa...")
+
+import math
+
+# ... existing code ... 
+n = int(input("Digite um número: "))
+print(f'O fatorial deste número é: [{math.factorial(n)}]')
+print("Encerrando o programa...") 
+
+"""
+
+print()
